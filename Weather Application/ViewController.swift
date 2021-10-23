8260810c69e49050428ca2c3c516b60b5ebd7781
MakariysBigys <makariys_big@icloud.com>
2021-10-23 14:21:58 +0300

    Add Conect with API and SearchBar

--- Weather Application/ViewController.swift
+++ Weather Application/ViewController.swift
@@ -7,14 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    print("Hello World!")
-        // Do any additional setup after loading the view.
+        
+        searchBar.delegate = self
+        //searchBar.showsCancelButton = true
     }
-
-
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let urlString = "https://api.openweathermap.org/data/2.5/forecast?q=\(searchBar.text!)&appid=59c2249fd161e32ba0eaa80a8259b136"
+        let url = URL(string: urlString)
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+        }
+        
+        task.resume()
+    }
 }
+    
 
+   
